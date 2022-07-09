dc3ced3f150c1a0a2b3c423620525fb15871e4d9
Simon Lenz <post@simon-lenz.de>
2022-07-09 22:47:24 +0200

    explore pandoc table more

--- plugins/InfoBox.hs
+++ plugins/InfoBox.hs
@@ -17,9 +17,32 @@ transformBlock (CodeBlock (_, classes, namevals) contents) | "infobox" `elem` cl
         ("ttable", [], [])
         (Caption (Just [(Str "some caption")]) [])
         [(AlignLeft,ColWidth 20), (AlignLeft,ColWidthDefault)]
-        (TableHead ("thead", [], []) [])
-        [TableBody ("tbody1", [], []) (RowHeadColumns 2) [Row ("trow1", [], []) [
-            Cell ("tcell1", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell1")]]
-        ]] []]
+        (TableHead ("thead", [], []) [
+            Row ("theadrow1", [], []) [
+                Cell ("theadcell1", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "Header col 1")]]
+            ]
+        ])
+        [TableBody ("tbody1", [], []) (RowHeadColumns 0) [
+            Row ("trow1.1", [], []) [
+                Cell ("tcell1.1a", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell1.1a"), (Str "2nd content cell1.1")]]
+            ],
+            Row ("trow1.2", [], []) [
+                Cell ("tcell1.2a", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell1.2a")]],
+                Cell ("tcell1.2b", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell1.2b")]]
+            ],
+            Row ("trow1.3", [], []) [
+                Cell ("tcell1.3a", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell1.3a")]],
+                Cell ("tcell1.3b", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell1.3b")]]
+            ]
+        ] [
+            Row ("trow2.1", [], []) [
+                Cell ("tcell2.1a", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell2.1a")]],
+                Cell ("tcell2.1b", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell2.1b")]]
+            ],
+            Row ("trow2.2", [], []) [
+                Cell ("tcell2.2a", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell2.2a")]],
+                Cell ("tcell2.2b", [], []) AlignLeft (RowSpan 1) (ColSpan 1) [Plain [(Str "content cell2.2b")]]
+            ]
+        ]]
         (TableFoot ("tfoot", [], []) [])
 transformBlock x = x
